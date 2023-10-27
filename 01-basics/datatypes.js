f52b30ca0df1ec4058e91fe7930c7eb1cbc59323
Nagendra Kuppala <38865666+NagendraKuppala@users.noreply.github.com>
2023-10-27 17:39:42 +0000

    datatypes

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //Treat all JS code as newer version/standards
+
+//Primitive datatypes:-
+let fname = "Nag" //String
+let age = 33 //number
+let isMale = true //boolean - true or false
+let salary; //undefined 
+//null datatype => standalone value => object datatype
+//symbol datatype => unique
+
+let person = {fname:"Nag",age:33,isMale:true} //object datatype
+
+console.log(typeof fname, typeof age, typeof isMale, typeof salary, typeof null, typeof person)
